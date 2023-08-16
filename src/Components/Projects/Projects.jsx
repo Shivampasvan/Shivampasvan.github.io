c06b75b2e9e3637f78a440abcd91e9c8b65cd85f
Shivampasvan <shivampasvan@gmail.com>
2023-08-16 20:34:09 +0530

    update

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,29 +1,23 @@
 import React from "react";
 import "./Projects.css";
-import ebay from "../../Assets/ebay.png"
-import amazon from "../../Assets/amazon.png"
-import geek from "../../Assets/geek.png"
-import ndtv from "../../Assets/ndtv.png"
+import ebay from "../../Assets/ebay.png";
+import amazon from "../../Assets/amazon.jpg";
+import geek from "../../Assets/geek.png";
+import ndtv from "../../Assets/ndtv.png";
 
 import { FaReact, FaHardHat } from "react-icons/fa";
 import {
-  SiBlockchaindotcom,
   SiHtml5,
-  
-  SiChai,
-  SiEthereum,
   SiExpress,
-  SiSocketdotio,
-  SiTailwindcss,
-  SiReactrouter,
   SiJavascript,
   SiMongodb,
-  SiNodedotjs
+  SiNodedotjs,
 } from "react-icons/si";
+
 import { DiCss3 } from "react-icons/di";
-import { VscJson } from "react-icons/vsc";
 import { IoLogoJavascript } from "react-icons/io";
-import { SiRedux } from "react-icons/si"
+import { SiRedux } from "react-icons/si";
+
 export const Projects = () => {
   return (
     <>
@@ -35,10 +29,7 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src={ebay}
-                  alt="SmartCart"
-                />
+                <img src={ebay} alt="SmartCart" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
@@ -46,9 +37,8 @@ export const Projects = () => {
               <p>
                 ebay was one of the first companies to create and market an
                 Internet Web site to match buyers and sellers of goods and
-                services. The company, which caters to individual sellers
-                and small businesses, is a market leader in e-commerce
-                worldwide.
+                services. The company, which caters to individual sellers and
+                small businesses, is a market leader in e-commerce worldwide.
               </p>
               <div>
                 <FaReact />
@@ -84,20 +74,16 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src={amazon}
-                  alt="Amazon"
-                />
+                <img src={amazon} alt="Amazon" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h3>Amazon (Clone)</h3>
               <p>
-                Amazon.com is a vast Internet-based enterprise that sells
-                books, music, movies, housewares, electronics, toys, and
-                many other goods, either directly or as the middleman
-                between other retailers and Amazon.com's millions of
-                customers.
+                Amazon.com is a vast Internet-based enterprise that sells books,
+                music, movies, housewares, electronics, toys, and many other
+                goods, either directly or as the middleman between other
+                retailers and Amazon.com's millions of customers.
               </p>
               <div>
                 <IoLogoJavascript />
@@ -108,7 +94,7 @@ export const Projects = () => {
               </div>
               <div>
                 <a
-                  href="https://peaceful-liger-051912.netlify.app/"
+                  href="https://project-lyart-zeta.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -133,24 +119,21 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                src={geek}
-                  alt="GeekBuying Clone"
-                />
+                <img src={geek} alt="GeekBuying Clone" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h3>Geekbuying (Clone) </h3>
               <p>
                 We at Geekbuying are all about making our customers' lives
-                comfortable and smarter. If you are looking for
-                the latest and the greatest product in categories like smart
-                home and outdoor gadgets, Geekbuying is where you should be!
+                comfortable and smarter. If you are looking for the latest and
+                the greatest product in categories like smart home and outdoor
+                gadgets, Geekbuying is where you should be!
               </p>
               <div>
                 <SiHtml5 />
                 <IoLogoJavascript />
-              <FaReact />
+                <FaReact />
                 <SiRedux />
               </div>
               <div>
@@ -180,19 +163,16 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src={ndtv}
-                  alt="NDTV"
-                />
+                <img src={ndtv} alt="NDTV" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h3>NDTV News (Clone)</h3>
               <p>
                 New Delhi Television Ltd is an Indian news media company
-                focusing on broadcast and digital news publication. The
-                company is considered to be a legacy brand that pioneered
-                independent news broadcasting in India.
+                focusing on broadcast and digital news publication. The company
+                is considered to be a legacy brand that pioneered independent
+                news broadcasting in India.
               </p>
               <div>
                 <SiJavascript />
